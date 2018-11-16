91e6bac6a0cb931aab518214cba84363d22b97d5
Nathan Arthur <nathan@nathanarthur.com>
2018-11-16 08:43:30 -0500

    Add css cache busting

--- compile-twig.php
+++ compile-twig.php
@@ -8,8 +8,8 @@ require_once __DIR__ . "/vendor/autoload.php";
 $loader = new Twig_Loader_Filesystem(__DIR__ . "/twig");
 $twig = new Twig_Environment($loader, array("debug" => true));
 
-//$data = json_decode(file_get_contents(__DIR__ . "/resume.json"), TRUE);
 $data = Yaml::parse(file_get_contents(__DIR__ . "/resume.yaml"));
+$data["cssVersion"] = hash("crc32", file_get_contents(__DIR__ . "/less/style.css"));
 
 $template = $twig->load("resume.twig");
 $html = $template->render(["data" => $data]);
