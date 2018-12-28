6beaaf0ec92de5d5dd557573909ebbd18fdda3a6
Nathan Arthur <nathan@nathanarthur.com>
2018-12-28 16:28:17 -0500

    compile resumes into build folder

--- compile-twig.php
+++ compile-twig.php
@@ -14,10 +14,21 @@ $twig = new Twig_Environment($loader, array("debug" => true));
 $data = Yaml::parse(file_get_contents(__DIR__ . "/resume.yaml"));
 $data["cssVersion"] = hash("crc32", file_get_contents(__DIR__ . "/less/style.css"));
 
+$buildDir = __DIR__ . "/build";
+delTree($buildDir);
+mkdir($buildDir);
 $twigPages = array_diff(scandir(__DIR__ . "/page"), [".",".."]);
-array_map(function($twigPage) use($data, $twig) {
+array_map(function($twigPage) use($data, $twig, $buildDir) {
   $template = $twig->load($twigPage);
   $html = $template->render(["data" => $data]);
   $filename = pathinfo($twigPage, PATHINFO_FILENAME);
-  file_put_contents(__DIR__ . "/$filename.html", $html);
+  file_put_contents("$buildDir/$filename.html", $html);
 }, $twigPages);
+
+function delTree($dir) {
+ $files = array_diff(scandir($dir), array('.','..'));
+  foreach ($files as $file) {
+    (is_dir("$dir/$file")) ? delTree("$dir/$file") : unlink("$dir/$file");
+  }
+  return rmdir($dir);
+}
