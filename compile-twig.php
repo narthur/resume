86a93e81b2571ac4da3ab829a8f02af30b4be0ba
Nathan Arthur <nathan@nathanarthur.com>
2018-12-28 13:12:04 -0500

    Compile everything in page/ dir

--- compile-twig.php
+++ compile-twig.php
@@ -5,13 +5,19 @@ use Symfony\Component\Yaml\Yaml;
 
 require_once __DIR__ . "/vendor/autoload.php";
 
-$loader = new Twig_Loader_Filesystem(__DIR__ . "/twig");
+$loader = new Twig_Loader_Filesystem([
+  __DIR__ . "/twig",
+  __DIR__ . "/page"
+]);
 $twig = new Twig_Environment($loader, array("debug" => true));
 
 $data = Yaml::parse(file_get_contents(__DIR__ . "/resume.yaml"));
 $data["cssVersion"] = hash("crc32", file_get_contents(__DIR__ . "/less/style.css"));
 
-$template = $twig->load("resume.twig");
-$html = $template->render(["data" => $data]);
-
-file_put_contents(__DIR__ . "/resume.html", $html);
+$twigPages = array_diff(scandir(__DIR__ . "/page"), [".",".."]);
+array_map(function($twigPage) use($data, $twig) {
+  $template = $twig->load($twigPage);
+  $html = $template->render(["data" => $data]);
+  $filename = pathinfo($twigPage, PATHINFO_FILENAME);
+  file_put_contents(__DIR__ . "/$filename.html", $html);
+}, $twigPages);
